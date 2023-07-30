a4057d3e8849240dc64ec15b687a49b4f13a7e88
mtopp <madstopp@gmail.com>
2023-07-30 22:22:50 +0200

    initial commit

---
+++ components/CustomButton.tsx
@@ -0,0 +1,32 @@
+"use client"
+
+import { CustomButtonProps } from "@/types";
+import Image from "next/image";
+
+const CustomButton = ( { title, containerStyles, handleClick, btnType, textStyles, rightIcon }: CustomButtonProps) => {
+  return (
+    <button
+      disabled={false}
+      type={btnType || "button"}
+      className={`custom-btn ${containerStyles}`}
+      onClick={handleClick}
+    >
+      <span className={`flex-1 ${textStyles}`}>
+        {title}
+      </span>
+      {rightIcon && (
+        <div className="relative w-8 h-8">
+          <Image 
+            src={rightIcon}
+            alt="right icon"
+            fill
+            className="object-contain"
+          />
+
+        </div>
+      )}
+    </button>
+  )
+}
+
+export default CustomButton
